a0b061b99a90b55cf96daec699bca1507ea4eddd
Vish0111 <it21315282@my.sliit.lk>
2024-03-01 10:07:29 +0530

    get user inputs

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const readline = require('readline');
+
 console.log('Hello World');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log('Hello World');
+
+rl.question('Enter your name: ', (name) => {
+    console.log(`Hello, ${name}!`);
+    rl.close();
+});
